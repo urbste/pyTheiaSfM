de71b61baf928bbb3a8d47d070c093eebf61c0d8
Steffen Urban <steffen.urban@zeiss.com>
2021-04-07 13:47:12 +0200

    feature matcher options
    Change-Id: I909d5e1d9f38456c010a1f78890980ad84ab7925

--- src/theia/matching/feature_matcher_options.h
+++ src/theia/matching/feature_matcher_options.h
@@ -46,6 +46,18 @@ struct FeatureMatcherOptions {
   // Number of threads to use in parallel for matching.
   int num_threads = 1;
 
+  // Matching may be performed in core (i.e. all in memory) or out-of-core. For
+  // the latter, features are written and read to/from disk as needed (utilizing
+  // an LRU cache). The out-of-core strategy is more scalable since the memory
+  // footprint is limited. Set this value to false to perform all-in-memory
+  // matching.
+  bool match_out_of_core = false;
+
+  // Keypoints and descriptors are stored to disk as they are added to the
+  // FeatureMatcher. Features will be stored in this directory, which must be a
+  // valid writeable directory.
+  std::string keypoints_and_descriptors_output_dir = "";
+
   // Only symmetric matches are kept.
   bool keep_only_symmetric_matches = true;
 
