794db2732ed8cd26ac3b3ca8abd8006afd8d93e4
Steffen Urban <urbste@googlemail.com>
2022-02-21 16:33:27 +0100

    update for single wheel docker build
    Change-Id: Ibd393afcf990c70c1fac7671ad4ac928f1ba5d5e

---
+++ pypackage/build-wheel-linux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+         #auditwheel repair "$wheel" --plat "$PLAT" -w /wheelhouse/
+         auditwheel repair "$wheel" -w /home/wheelhouse/ --no-update-tags
+    fi
+}
+
+uname -a
+echo "Current CentOS Version:"
+cat /etc/centos-release
+
+set -e -u -x
+
+cd home
+mkdir -p /home/wheelhouse
+
+# we cannot simply use `pip` or `python`, since points to old 2.7 version
+PYBIN="/opt/python/$PYTHON_VERSION/bin"
+PYVER_NUM=$($PYBIN/python -c "import sys;print(sys.version.split(\" \")[0])")
+PYTHONVER="$(basename $(dirname $PYBIN))"
+
+echo "Python bin path: $PYBIN"
+echo "Python version number: $PYVER_NUM"
+echo "Python version: $PYTHONVER"
+
+export PATH=$PYBIN:$PATH
+
+${PYBIN}/pip install auditwheel
+
+PLAT=manylinux2014_x86_64
+"${PYBIN}/python" setup.py bdist_wheel --plat-name=$PLAT
+
+cp /home/dist/*.whl /home/wheelhouse
+# Bundle external shared libraries into the wheels
+for whl in /home/wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+ls -ltrh /home/wheelhouse/
