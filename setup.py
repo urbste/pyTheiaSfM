b20467c080727b1fe5d2408be5ea50616de7e758
Urban Steffen ZOSURBAN <zosurban@zjlxq00047.deje2.zeiss.org>
2022-03-04 13:49:08 +0100

    bump up python verion

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ create_package()
 
 setuptools.setup(
     name='pytheia',
-    version='0.1.4',
+    version='0.1.5',
     description='A performant Structure from Motion library for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
