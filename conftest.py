1665c575937c4acc7c07cc3637bf9db3576f1a67
steffen <urbste@googlemail.com>
2025-04-08 17:30:14 +0200

    remove tests as just too much code for nothing
    Change-Id: I2365caa0eb7262d569c78afe67de695b1db9b68f

---
+++ conftest.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+# Add the test directory to Python path
+test_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, test_dir)
--- tests/sfm/noise_utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def add_noise_to_point(points_2d, noise_std):
-    points_2d_noisy = points_2d + \
-        np.random.randn(points_2d.shape[0], points_2d.shape[1]) * noise_std
-    return points_2d_noisy
