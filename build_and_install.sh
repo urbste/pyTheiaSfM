b22f620e54747b057fe63cbef14cc105da3df117
steffen <urbste@googlemail.com>
2025-09-01 07:20:07 +0200

    update stub generation
    Change-Id: I46f2f2dbd81536da81e5c67e935da23fd2b6216d

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,7 +1,6 @@
 rm src/pytheia/*.so
 rm cmake_build
 rm dist/
-# remove egg
 
 BUILD_MARCH_NATIVE=0 python setup.py bdist_wheel --plat-name=manylinux_2_35_x86_64
 cd dist && pip install --force-reinstall *.whl 
